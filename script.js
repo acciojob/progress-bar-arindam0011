30ce975454b519389855c5e6bbd1e6f02e9a4d36
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-02 00:00:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',(event)=>{
+    const circle=document.getElementsByClassName("circle");
+    const line=document.getElementsByClassName("line");
+    
+    const next=document.getElementById('next');
+    const prev=document.getElementById('prev');
+    let count=0;
+    next.addEventListener("click", forward);
+    prev.addEventListener("click", backward);
+
+    
+    function forward(){
+        count++;
+        if(count>0 && count<circle.length){
+           circle[count].classList.add('active');
+           line[count-1].classList.add('active');
+        }
+        
+    }
+
+    function backward(){
+        count--;
+        circle[count+1].classList.remove('active');
+           line[count].classList.remove('active');
+        if(count>0 && count<circle.length){
+           
+           circle[count].classList.add('active');
+           line[count-1].classList.add('active');
+        }
+    }
+
+})
